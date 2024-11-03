a920199decd76d543a2257fc6382f35ebb6d1ac9
JustinF21 <justmarti100@icloud.com>
2024-11-03 16:44:42 -0600

    first commit

---
+++ app/config/env.js
@@ -0,0 +1,15 @@
+const env = {
+    database: 'ejemplo3',
+    username: 'ejemplo3_user',
+    password: 'MMkjuAMXIXvT7XlW7sCZ7A7PQaK4SxiJ',
+    host: 'dpg-cruupk68ii6s738i4jkg-a.oregon-postgres.render.com',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
+  module.exports = env;
