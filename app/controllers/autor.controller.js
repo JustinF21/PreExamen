2d3b870d5b1b07a069bc146ea89ba2b5d36948ce
JustinF21 <justmarti100@icloud.com>
2024-11-03 17:57:58 -0600

    editcontr

--- app/controllers/autor.controller.js
+++ app/controllers/autor.controller.js
@@ -1,125 +1,145 @@
 const db = require('../config/db.config.js');
-const Autor = db.Autor;
+const Huesped = db.Huesped;
 
 exports.create = (req, res) => {
-    let autor = {};
+    let huesped = {};
 
     try {
-        autor.nombre = req.body.nombre;
-        autor.apellido = req.body.apellido;
-        autor.nacionalidad = req.body.nacionalidad;
-        autor.fecha_nacimiento = req.body.fecha_nacimiento;
+        huesped.nombre = req.body.nombre;
+        huesped.apellido = req.body.apellido;
+        huesped.documento_cantidad = req.body.documento_cantidad;
+        huesped.Telefono = req.body.Telefono;
+        huesped.correo_electronico = req.body.correo_electronico; // Asegúrate de que el nombre del campo coincida
+        huesped.habitacion = req.body.habitacion;
 
-        Autor.create(autor).then(result => {
-            res.status(200).json({
-                message: "autor creado exitosamente con id = " + result.id_autor,
-                autor: result,
+        Huesped.create(huesped).then(result => {
+            res.status(201).json({
+                message: "Huésped creado exitosamente con id = " + result.id_Huesped,
+                huesped: result,
+            });
+        }).catch(error => {
+            res.status(500).json({
+                message: "¡Fallo al crear el huésped!",
+                error: error.message
             });
         });
     } catch (error) {
         res.status(500).json({
-            message: "¡Fallo al crear el autor!",
+            message: "¡Fallo al crear el huésped!",
             error: error.message
         });
     }
 };
 
-exports.retrieveAllEmpleados = (req, res) => {
-    Empleado.findAll()
-        .then(empleadoInfos => {
+exports.retrieveAllHuespedes = (req, res) => {
+    Huesped.findAll()
+        .then(huespedInfos => {
             res.status(200).json({
-                message: "¡Empleados obtenidos exitosamente!",
-                empleados: empleadoInfos
+                message: "¡Huéspedes obtenidos exitosamente!",
+                huespedes: huespedInfos
             });
         })
         .catch(error => {
-            console.log(error);
+            console.error(error);
             res.status(500).json({
-                message: "¡Error al obtener los empleados!",
-                error: error
+                message: "¡Error al obtener los huéspedes!",
+                error: error.message
             });
         });
 };
 
-exports.getAutorById = (req, res) => {
-    let autorId = req.params.id;
-    Autor.findByPk(autorId)
-        .then(autor => {
+exports.getHuespedById = (req, res) => {
+    let huespedId = req.params.id;
+    Huesped.findByPk(huespedId)
+        .then(huesped => {
+            if (!huesped) {
+                return res.status(404).json({
+                    message: "No se encontró el huésped con id = " + huespedId,
+                    error: "404"
+                });
+            }
             res.status(200).json({
-                message: "autor obtenido exitosamente con id = " + autorId,
-                autor: autor
+                message: "Huésped obtenido exitosamente con id = " + huespedId,
+                huesped: huesped
             });
         })
         .catch(error => {
-            console.log(error);
+            console.error(error);
             res.status(500).json({
-                message: "¡Error al obtener autor con id!",
-                error: error
+                message: "¡Error al obtener huésped con id!",
+                error: error.message
             });
         });
 };
 
 exports.updateById = async (req, res) => {
+    let huespedId = req.params.id;
+
     try {
-        let autorId = req.params.id;
-        let autor = await Autor.findByPk(autorId);
-    
-        if (!autor) {
-            res.status(404).json({
-                message: "No se encontró el autor para actualizar con id = " + autorId,
-                autor: "",
+        let huesped = await Huesped.findByPk(huespedId);
+
+        if (!huesped) {
+            return res.status(404).json({
+                message: "No se encontró el huésped para actualizar con id = " + huespedId,
                 error: "404"
             });
-        } else {    
-            let updatedObject = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido,
-                nacionalidad: req.body.nacionalidad,  
-                fecha_nacimiento: req.body.fecha_nacimiento
-            }
-            let result = await Autor.update(updatedObject, {returning: true, where: {id_autor: autorId}});
-            
-            if (!result) {
-                res.status(500).json({
-                    message: "No se puede actualizar un autor con id = " + req.params.id,
-                    error: "No se pudo actualizar el autor",
-                });
-            };
+        }
 
-            res.status(200).json({
-                message: "Actualización exitosa de un autor con id = " + autorId,
-                autor: updatedObject,
+        let updatedObject = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            documento_cantidad: req.body.documento_cantidad,
+            Telefono: req.body.Telefono,
+            correo_electronico: req.body.correo_electronico,
+            habitacion: req.body.habitacion
+        };
+
+        const [updatedRowCount, updatedRows] = await Huesped.update(updatedObject, {
+            returning: true,
+            where: { id_Huesped: huespedId }
+        });
+
+        if (updatedRowCount === 0) {
+            return res.status(500).json({
+                message: "No se pudo actualizar el huésped con id = " + req.params.id,
+                error: "No se pudo actualizar el huésped",
             });
         }
+
+        res.status(200).json({
+            message: "Actualización exitosa de un huésped con id = " + huespedId,
+            huesped: updatedRows[0],
+        });
     } catch (error) {
         res.status(500).json({
-            message: "No se puede actualizar un autor con id = " + req.params.id,
+            message: "No se puede actualizar un huésped con id = " + req.params.id,
             error: error.message
         });
     }
 };
 
 exports.deleteById = async (req, res) => {
-    try {
-        let autorId = req.params.id;
-        let autor = await Autor.findByPk(autorId);
+    let huespedId = req.params.id;
 
-        if (!autor) {
-            res.status(404).json({
-                message: "No existe el autor con id = " + autorId,
+    try {
+        let huesped = await Huesped.findByPk(huespedId);
+
+        if (!huesped) {
+            return res.status(404).json({
+                message: "No existe el huésped con id = " + huespedId,
                 error: "404",
             });
-        } else {
-            await autor.destroy();
-            res.status(200).json({
-                message: "Eliminación exitosa del autor con id = " + autorId,
-                autor: autor,
-            });
         }
+
+        await huesped.destroy();
+        res.status(200).json({
+            message: "Eliminación exitosa del huésped con id = " + huespedId,
+            huesped: huesped,
+        });
     } catch (error) {
         res.status(500).json({
-            message: "No se puede eliminar un autor con id = " + req.params.id,
+            message: "No se puede eliminar un huésped con id = " + req.params.id,
             error: error.message,
         });
     }
-}
+};
